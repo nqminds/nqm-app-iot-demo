9b4ccc914e282e1e84ffd2ef755165d038f0970d
Alex <mereacre@gmail.com>
2016-12-09 15:36:37 +0000

    Added default popup open

--- client/modules/buses/components/bus-map.js
+++ client/modules/buses/components/bus-map.js
@@ -1,16 +1,43 @@
 import React from "react";
 import {Map, TileLayer, Marker, Popup} from "react-leaflet";
 import * as _ from "lodash";
+const MK_COORDINATES = [52.0295016, -0.7766588];
+
+// Create your own class, extending from the Marker class.
+class ExtendedMarker extends Marker {
+  constructor() {
+    super();
+  }
+
+	// "Hijack" the component lifecycle.
+  componentDidMount() {
+    // Call the Marker class componentDidMount (to make sure everything behaves as normal)
+    super.componentDidMount();
+
+    // Access the marker element and open the popup.
+    if (this.props.open)
+      this.leafletElement.openPopup();
+  }
+}
+
+ExtendedMarker.propTypes = {
+  open: React.PropTypes.bool.isRequired,
+};
 
 const BusMap = ({busMetadata, data, currentBusID, style}) => {
-  let center = [52.0295016, -0.7766588];
+  let center = MK_COORDINATES;
+  let open;
 
   const listMarker = _.map(data, (val) => {
-    if (currentBusID === val.ID)
+    if (currentBusID === val.ID) {
       center = [val.lat, val.lon];
+      open = true;
+    } else
+      open = false;
 
     return (
-      <Marker
+      <ExtendedMarker
+        open={open}
         key={val.ID}
         position={[val.lat, val.lon]}
         clickable="true"
@@ -20,7 +47,7 @@ const BusMap = ({busMetadata, data, currentBusID, style}) => {
         <Popup>
           <span>{busMetadata[val.ID].Title}</span>
         </Popup>
-      </Marker>);
+      </ExtendedMarker>);
   });
 
   const map = (
