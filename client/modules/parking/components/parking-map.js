5b8e1bf7d639f6def22ed76937407b6eec88529b
Alex <mereacre@gmail.com>
2017-02-06 17:05:57 +0000

    Added zoom

--- client/modules/parking/components/parking-map.js
+++ client/modules/parking/components/parking-map.js
@@ -4,16 +4,24 @@ import MarkerCluster from "./marker-cluster";
 
 const MK_COORDINATES = [52.0295016, -0.7766588];
 
-const ParkingMap = ({parkingMetadata, data, clickMarker, style}) => {
+const ParkingMap = ({parkingMetadata, data, clickMarker, style, currentParkingID}) => {
   const markerComponent = (
     <MarkerCluster
       parkingMetadata={parkingMetadata}
       data={data}
       handleClickMarker={clickMarker}
     />);
+  let centreCoordinates = MK_COORDINATES;
+  let zoom = 12;
+
+  // Add the centre coordinates of the parking space
+  if (currentParkingID) {
+    zoom = 18;
+    centreCoordinates = [parkingMetadata[currentParkingID].Latitude, parkingMetadata[currentParkingID].Longitude];
+  }
 
   const map = (
-    <Map center={MK_COORDINATES} zoom={12} >
+    <Map center={centreCoordinates} zoom={zoom} >
       <TileLayer
         url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
         attribution='Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> \
@@ -32,6 +40,7 @@ const ParkingMap = ({parkingMetadata, data, clickMarker, style}) => {
 
 ParkingMap.propTypes = {
   clickMarker: React.PropTypes.func.isRequired,
+  currentParkingID: React.PropTypes.number.isRequired,
   data: React.PropTypes.array.isRequired,
   parkingMetadata: React.PropTypes.object.isRequired,
   style: React.PropTypes.object,
